076e74fef0d4f103ec25f744090319ee6f6aadbe
xpudding <69681778+pessoa736@users.noreply.github.com>
2024-03-25 23:27:55 -0300

    Update main.php

--- scr/main.php
+++ scr/main.php
@@ -2,8 +2,14 @@
 
 require_once("classAluno.php");
 
+// criando um novo aluno
 $Davi = new aluno("Davi dos Santos Passos");
 
+// colocando as notas no objeto do aluno
 $Davi->setNotas([10, 2, 10, 9]);
+
+// calculando a media
 $Davi->calculeAMedia() ;
+
+// mostrando a situação do aluno :/
 $Davi->mostarDadosDoAluno() ;
